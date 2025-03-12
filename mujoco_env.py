8bbba158d10b6e0775f9a9ed001b00692965dc9c
FennMai <fennmaicontact@gmail.com>
2025-03-12 19:34:06 +0800

    无关紧要

--- mujoco_env.py
+++ mujoco_env.py
@@ -312,7 +312,7 @@ class MujocoSim:
 
 class MujocoEnv:
     def __init__(self, render_images=True, show_viewer=True, show_images=False):
-        self.mjcf_path = 'models/stanford_tidybot/room_scene.xml'
+        self.mjcf_path = 'models/stanford_tidybot/scene.xml'
         self.render_images = render_images
         self.show_viewer = show_viewer
         self.show_images = show_images
